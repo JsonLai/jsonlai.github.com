d1822efa664a5b63a52aba895f7c1928a7a83d41
赖泽城 <laizecheng@live.com>
2020-03-27 20:30:01 +0800

    初始化

---
+++ baidu-statistics.js
@@ -0,0 +1,13 @@
+var _hmt = _hmt || []
+;(function() {
+  let hm = document.createElement('script')
+  hm.src = '//hm.baidu.com/hm.js?4b1cfd42e49e6bcbb423515a1ab52097'
+  let s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
+;(function() {
+  let hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?1d8137b1d22392852da8458ac7611118'
+  let s = document.getElementsByTagName('script')[0]
+  s.parentNode.insertBefore(hm, s)
+})()
